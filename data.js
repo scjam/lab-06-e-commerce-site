ef3735d2b3be0a9e630d7b73ff4b48aa068e03ba
Sydney James <sydneyjames7@gmail.com>
2020-10-05 14:06:03 -0700

    build out data.js with products

---
+++ data.js
@@ -0,0 +1,42 @@
+export const drinks = [
+    {
+        id: 'pinaColada',
+        name: 'Piña colada',
+        image: '',
+        description: 'A blended pineapple and coconut classic, originated in Puerto Rico.',
+        liquor: 'light rum',
+        price: 8,
+    },
+    {
+        id: 'singaporeSling',
+        name: 'Singapore Sling',
+        image: '',
+        description: 'Created by Ngiam Tong Boon around 1915, this is one of the few classic tiki cocktails made without rum.',
+        liquor: 'gin',
+        price: 9,
+    },
+    {
+        id: 'painkiller',
+        name: 'Painkiller',
+        image: '',
+        description: 'Pineapple, orange, high-proof rum and nutmeg make this concoction from the 1970s a lasting favorite.',
+        liquor: 'dark rum',
+        price: 10,
+    },
+    {
+        id: 'junglebird',
+        name: 'Junglebird',
+        image: '',
+        description: 'Another 1970s cocktail, hailing from Kuala Lumpur, that has the surprising addition of Campari.',
+        liquor: 'dark rum',
+        price: 9,
+    },
+    {    
+        id: 'zombie',
+        name: 'Zombie',
+        image: '',
+        description: 'Created by Don Beach in 1934, this cocktail can best be described as high-octane rocket fuel disguised as fruit punch.',
+        liquor: 'dark rum',
+        price: 12,
+    },
+];
