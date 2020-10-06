1615f2fcf0fc20f568fc3f652e236a4b855ffa33
Sydney James <sydneyjames7@gmail.com>
2020-10-06 15:12:37 -0700

    test worked so ACP

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,4 +1,5 @@
 import { renderTiki } from '../utils.js';
+import { renderTableRow } from '../cart/cart-utils.js';
 
 const test = QUnit.test;
 
@@ -15,6 +16,19 @@ test('should take in a drink and return a li with relevant information', (expect
     const expected = '<li id="pinaColada" class="drink"><h3 class="name">Piña colada</h3><img class="image" src="../assets/pinaColada.jpg"><p class="description">A blended pineapple and coconut classic, originated in Puerto Rico.</p><p class="price">$8.00</p><button value="pinaColada">Add</button></li>';
 
     const actual = renderTiki(drink);
+
+    expect.equal(actual.outerHTML, expected);
+});
+
+test('should take in a cart item and return a tr element with appropriate contents', (expect) => {
+    const cartItem = {
+        id: 'zombie',
+        quantity: 2,        
+    };
+
+    const expected = '<tr><td>Zombie</td><td>$12</td><td>2</td><td>$24</td></tr>';
+
+    const actual = renderTableRow(cartItem);
 
     expect.equal(actual.outerHTML, expected);
 });
